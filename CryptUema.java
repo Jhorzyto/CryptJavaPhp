8744b432e012eef5f57a563d944d9bfcb49e746b
Jhordan Lima <jhorzyto@gmail.com>
2017-11-30 22:49:39 -0300

    Update CryptUema.java

--- CryptUema.java
+++ CryptUema.java
@@ -9,7 +9,7 @@ public class CryptUema {
     static char[] HEX_CHARS = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 
     private String ivParams     = "9876543210fedcba";
-    private String chaveSecreta = "sigUema.12qwaszx";
+    private String chaveSecreta = "sigUema";
     private IvParameterSpec ivParamsSpec;
     private SecretKeySpec keySpec;
     private Cipher cipher;
